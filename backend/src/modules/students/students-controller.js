19e21e47df72767659d202f31b1c2d3808912317
Angelino Oliveira <lino.pires@live.com>
2025-06-09 18:24:26 -0300

    feat: implement students-controller funcionalities

--- backend/src/modules/students/students-controller.js
+++ backend/src/modules/students/students-controller.js
@@ -2,28 +2,55 @@ const asyncHandler = require("express-async-handler");
 const { getAllStudents, addNewStudent, getStudentDetail, setStudentStatus, updateStudent } = require("./students-service");
 
 const handleGetAllStudents = asyncHandler(async (req, res) => {
-    //write your code
-
+    const { name, className, section, roll } = req.query;
+    const students = await getAllStudents({ name, className, section, roll });
+    res.json({ students });
 });
 
 const handleAddStudent = asyncHandler(async (req, res) => {
-    //write your code
+    const payload = req.body;
 
+    try {
+        const newStudent = await addNewStudent(payload);
+        res.status(201).json({ student: newStudent });
+    } catch (err) {
+        res.status(err.statusCode || 500).json({
+        error: err.message || "Unexpected error occurred",
+        });
+    }
 });
 
 const handleUpdateStudent = asyncHandler(async (req, res) => {
-    //write your code
+    const { id: userId } = req.params;
+    const payload = req.body;
 
+    try {
+        const message = await updateStudent({ ...payload, userId: Number(userId) });
+        res.json(message);
+    } catch (err) {
+        res.status(err.statusCode || 500).json({
+        error: err.message || "Unexpected error occurred",
+        });
+    }
 });
 
 const handleGetStudentDetail = asyncHandler(async (req, res) => {
-    //write your code
-
+    const { id } = req.params;
+    const student = await getStudentDetail(Number(id));
+    if (!student) {
+        return res.status(404).json({ message: "Student not found" });
+    }
+    res.json(student);
 });
 
 const handleStudentStatus = asyncHandler(async (req, res) => {
-    //write your code
-
+    const { id } = req.params;
+    const { status } = req.body;
+    const updated = await setStudentStatus({ userId: Number(id), status});
+    if (!updated) {
+        return res.status(404).json({ message: "Student not found or status not updated" });
+    }
+    res.json({ success: true });
 });
 
 module.exports = {
